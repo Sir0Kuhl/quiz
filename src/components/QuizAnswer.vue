9ef0d77be787e1a56bed3da26f15e064f86303be
Luigi Junge <luigijunge@Mini-von-Luigi.fritz.box>
2022-05-03 19:02:21 +0200

    first commit, quiz app

---
+++ src/components/QuizAnswer.vue
@@ -0,0 +1,17 @@
+<template>
+  <li
+    v-bind:class="{
+      rightAnswer: index == selectedAnswer && index == rightAnswer,
+      wrongAnswer: index == selectedAnswer && index != rightAnswer,
+    }"
+    @click="selectAnswer(index)"
+  >
+    {{ answer }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["index", "answer", "selectedAnswer", "rightAnswer", "selectAnswer"],
+};
+</script>
